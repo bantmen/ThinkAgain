9eeb3decb04e897912b883389bcf9915f1270550
cberkay <c.berkayantmen@gmail.com>
2014-12-22 14:37:24 +0200

    using states and calling the update function periodically

--- background.js
+++ background.js
@@ -1,17 +1,21 @@
+var MINUTE = 60000;
+
 /*
-* Listns for the requests from check.js.
+* Listens for the requests from check.js.
 */
 chrome.runtime.onMessage.addListener(
 	function(message, sender, sendResponse) {
+		var action = message.action;
+		var url = message.url;
 		console.log('received message:');
-		console.log(message.action);
-		if (message.action == 'resume_timer') {
-			resume_timer();
+		console.log(action);
+		if (action == 'resume_timer') {
+			resume_timer(url);
 		}
-		else if (message.action == 'pause_timer') {
-			pause_timer();
+		else if (action == 'pause_timer') {
+			pause_timer(url);
 		}
-		else if (message.action == 'close_current_tab') {
+		else if (action == 'close_current_tab') {
 			close_current_tab();
 		}
 		else {
@@ -21,13 +25,15 @@ chrome.runtime.onMessage.addListener(
 });
 
 // Resumes the timer for a listed website
-function resume_timer() {
-
+function resume_timer(url) {
+	localStorage['state'] = 'resumed';
+	sendResponse({reaction: 'true'});
 }
 
-// Pauses the timer for a listed websit
-function pause_timer() {
-
+// Pauses the timer for a listed website and saves it
+function pause_timer(url) {
+	localStorage['state'] = 'paused';
+	sendResponse({reaction: 'true'});
 }
 
 // Closes the current active tab
@@ -40,3 +46,27 @@ function close_current_tab() {
 		});		
 	});
 }
+
+function update_timer(url) {
+	if (!localStorage['state'] == 'resumed') {
+		return ;
+	}
+	chrome.storage.sync.get({time_dict: {}}, function(result) {
+		var timers = result.time_dict;
+		console.log(timers);
+		var current_timer;
+		// Continue from the previous timer
+		if (timers[url]) {
+			console.log('cont timer');
+			current_timer = timers[url];
+		}
+		// If doesn't exist, then create a new one
+		else {
+			console.log('new timer');
+			current_timer = 0;
+		}
+	});
+}
+
+// Force an update of the counter every minute. 
+window.setInterval(update_timer, MINUTE);
