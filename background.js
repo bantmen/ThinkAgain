cbd970e1c31dd1aa7cd531e0b816b0e2a4502080
cberkay <c.berkayantmen@gmail.com>
2014-12-22 11:11:55 +0200

    fixed tab close message call

--- background.js
+++ background.js
@@ -1,17 +1,22 @@
 chrome.runtime.onMessage.addListener(
 	function(message, sender, sendResponse) {
-		console.log('received message');
-		console.log(message.greeting);
+		console.log('received message:');
+		console.log(message.action);
 		if (message.action == 'close_current_tab') {
+			console.log('inside if');
 			chrome.tabs.query({
 				active: true, currentWindow: true
 			}, function(tabs) {
-				sendResponse({reaction: 'a'});
+				console.log('reached');
+				chrome.tabs.remove(tabs[0].id, function() {
+					sendResponse({reaction: 'true'});
+				});		
 			});
-			sendResponse({reaction:'closed the tab'});
+			//sendResponse({reaction:'closed the tab'});
 		}
 		else {
+			console.log('insid else');
 			sendResponse({reaction:'unknown action'});
 		}
-		
+		return true;
 });
