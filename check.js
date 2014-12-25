5507f822ceac0bc3605f3e18e438b35f42c1ccc5
cberkay <c.berkayantmen@gmail.com>
2014-12-25 02:22:07 +0200

    using callbacks for confirmation, working now

--- check.js
+++ check.js
@@ -12,41 +12,44 @@ function check_page(url) {
 			console.log(current);
 			if (url.indexOf(current) > -1) {
 				console.log('is monitored');
-				var confirm_string = get_confirmation_string(current);
-				if (confirm(confirm_string)) {
-					console.log('cont.');
-					chrome.runtime.sendMessage({
-						action: "resume_timer", url: url 
-					}, function(response) {
-				  		console.log('response:');
-				  		console.log(response.reaction);
-					});
-				}
-				else {
-					console.log('not cont.');
-					chrome.runtime.sendMessage({
-						action: "close_current_tab", url: url
-					}, function(response) {
-				  		console.log('response:');
-				  		console.log(response.reaction);
-					});
-				}
-				return 1;
+				confirm_close(current, confirm_proceed);
 			}
 		}
 		console.log('not monitored');
-		return 0;
+		// return 0;
 	});
 }
 
-function get_confirmation_string(url) {
+function confirm_proceed(confirmed) {
+	if (confirmed) {
+		console.log('cont.');
+		chrome.runtime.sendMessage({
+			action: "resume_timer", url: url 
+		}, function(response) {
+	  		console.log('response:');
+	  		console.log(response.reaction);
+		});
+	}
+	else {
+		console.log('not cont.');
+		chrome.runtime.sendMessage({
+			action: "close_current_tab", url: url
+		}, function(response) {
+	  		console.log('response:');
+	  		console.log(response.reaction);
+		});
+	}
+}
+
+function confirm_close(url, callback) {
 	chrome.storage.sync.get({time_dict: {}, date_time_dict: {}}, 
 		function(result) {
 			var timers = result.time_dict;
 			var date_timers = result.date_time_dict;
 			var now = new Date().toJSON().slice(0,10);
 			var today = new Date();
-			var today_spent = ((date_timers[now] ? date_timers[now][url] : "0.00")/1000/60/60).toFixed(2); 
+			var today_spent = ((date_timers[now] ? date_timers[now][url] : "0.00")/1000/60/60).toFixed(2);
+			if (isNaN(today_spent)) today_spent = "0.00"; // if date exists, but url doesnt
 			var week_spent = 0;
 			var cur_ms;
 			var cur_date;
@@ -62,11 +65,17 @@ function get_confirmation_string(url) {
 			}
 			week_spent = week_spent.toFixed(2);
 			var str_builder = [];
-			str_builder.push('You spent ', today_spent, ' hours today,', 
+			str_builder.push('You spent ', today_spent, ' hours today, ', 
 							'and ', week_spent, ' hours this week on ',
 							url, '. Are you sure that you want to continue?');
-			console.log(str_builder.join(""));
-			return str_builder.join("");
+			var confirm_string = str_builder.join("");
+			console.log(confirm_string);
+			if (confirm(confirm_string)) {
+				callback(true);
+			}
+			else {
+				callback(false);
+			}
 		}
 	);
 }
